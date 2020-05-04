5100ea0eecc435989b07f012e170f1d148975429
mholwill <m_holwill@hotmail.co.uk>
2020-05-04 10:07:32 +0100

    Finished unit testing

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -17,10 +17,48 @@ describe('calculator', function () {
     assert.strictEqual(5, calculator.runningTotal)
   })
 
-  it('should be able to subtratc two numbers', function(){
+  it('should be able to subtract two numbers', function(){
     calculator.previousTotal = 7;
     calculator.subtract(4)
     assert.strictEqual(3, calculator.runningTotal)
   })
+
+  it('should be able to multiply two numbers', function(){
+    calculator.previousTotal = 5;
+    calculator.multiply(3)
+    assert.strictEqual(15, calculator.runningTotal)
+  })
+
+  it('should be able to divide two numbers', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.strictEqual(3, calculator.runningTotal)
+  })
+
+  it('should be able to recognise what number is clicked', function(){
+    calculator.numberClick(9);
+    assert.strictEqual(9, calculator.runningTotal)
+  })
+
+  it('should be able to chain multiple operations together', function(){
+    calculator.numberClick(5);
+    calculator.operatorClick('+');
+    calculator.numberClick(5);
+    calculator.operatorClick('=');
+    assert.strictEqual(10, calculator.runningTotal);
+  })
+
+  it('should be able to chain multiple operations together', function(){
+    calculator.numberClick(5);
+    calculator.operatorClick('+');
+    calculator.numberClick(5);
+    calculator.operatorClick('=');
+    calculator.clearClick();
+    const previousTotal = calculator.previousTotal;
+    assert.equal(10, previousTotal)
+    assert.strictEqual(0, calculator.runningTotal);
+  })
+
+
   
 });
